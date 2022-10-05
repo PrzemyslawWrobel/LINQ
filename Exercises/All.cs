873e4cac9286cde2c48c4a758587d4eef9b77f9f
PrzemyslawWrobel <przemek.wrobel@poczta.fm>
2022-10-05 10:04:00 +0200

    start

---
+++ Exercises/All.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Exercises
+{
+    public static class All
+    {
+        //Coding Exercise 1
+        //Use LINQ to implement the AreAllNumbersDivisibleBy10 method,
+        //which checks if all numbers in the collection are divisible by 10.
+        public static bool AreAllNumbersDivisibleBy10(int[] numbers)
+        {
+           return numbers.All(x => x % 10 == 0);
+        }
+
+        //Coding Exercise 2
+        //Using LINQ, implement the AreAllPetsOfTheSameType method that checks
+        //if all Pets in the collection are of the same PetType.
+        public static bool AreAllPetsOfTheSameType(IEnumerable<Pet> pets)
+        {
+            return 
+                pets.All(x => x.PetType == PetType.Dog) ||
+                pets.All(x => x.PetType == PetType.Cat) ||
+                pets.All(x => x.PetType == PetType.Fish);
+        }
+
+        //Refactoring challenge
+        //TODO implement this method
+        public static bool AreAllWordsOfTheSameLength_Refactored(List<string> words)
+        {
+            return
+                words.All(x => x.Length == 0) ||
+                words.All(x => x.Length == 1) ||
+                words.All(x => x.Length == words[0].Length);
+
+        }
+
+        //do not modify this method
+        public static bool AreAllWordsOfTheSameLength(List<string> words)
+        {
+            if (words.Count == 0 || words.Count == 1)
+            {
+                return true;
+            }
+            var length0 = words[0].Length;
+            for (int i = 1; i < words.Count; ++i)
+            {
+                if (words[i].Length != length0)
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        public enum PetType
+        {
+            Cat,
+            Dog,
+            Fish
+        }
+
+        public class Pet
+        {
+            public int Id { get; }
+            public string Name { get; }
+            public PetType PetType { get; }
+            public float Weight { get; }
+
+            public Pet(int id, string name, PetType petType, float weight)
+            {
+                Id = id;
+                Name = name;
+                PetType = petType;
+                Weight = weight;
+            }
+
+            public override string ToString()
+            {
+                return $"Id: {Id}, Name: {Name}, Type: {PetType}, Weight: {Weight}";
+            }
+        }
+    }
+}
---
+++ Exercises/OrderBy.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Exercises
+{
+    public static class OrderBy
+    {
+        //Coding Exercise 1
+        /*
+         Using LINQ, implement the OrderFromLongestToShortest method, which takes 
+        a collection of strings, and returns those strings ordered from 
+        longest to shortest.
+        For example, for {"bb", "a", "ccc"} the result should be {"ccc", "bb", "a"}
+         */
+        public static IEnumerable<string> OrderFromLongestToShortest(
+            IEnumerable<string> words)
+        {
+            return words.OrderBy(x => x.Length).Reverse();
+        }
+
+        //Coding Exercise 2
+        /*
+         Using LINQ, implement the FirstEvenThenOddDescending method, 
+         which orders numbers like this:
+            *first, the even numbers
+            *then, the odd numbers
+         Then the numbers should be ordered descending. 
+         For example, for numbers {1,2,3,4,5,6,7} the result should be: {6,4,2,7,5,3,1}.
+         */
+        public static IEnumerable<int> FirstEvenThenOddDescending(
+            IEnumerable<int> numbers)
+        {
+            return numbers.OrderBy(number => number % 2 != 0)
+                .ThenByDescending(number => number);
+        }
+
+        //Refactoring challenge
+        //TODO implement this method
+        public static IEnumerable<DateTime> OrderByMonth_Refactored(List<DateTime> dates)
+        {
+          return dates.OrderBy(date => date.Month);
+        }
+
+        //do not modify this method
+        public static IEnumerable<DateTime> OrderByMonth(List<DateTime> dates)
+        {
+            dates.Sort((left, right) =>
+            {
+                return left.Month.CompareTo(right.Month);
+            });
+            return dates;
+        }
+    }
+}
